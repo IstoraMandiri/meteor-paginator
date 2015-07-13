85ac91afffed4e2aa9f7b8561f8067efd7452d6b
Chris Hitchcott <hitchcott@gmail.com>
2015-07-14 01:16:38 +0800

    version up

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'hitchcott:paginator',
   summary: 'Super simple client-only cursor pagination + UI',
-  version: '0.2.1',
+  version: '0.2.2',
   git: 'https://github.com/hitchcott/meteor-paginator'
 });
 
